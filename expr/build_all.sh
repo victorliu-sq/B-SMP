ba76f6f19e93f453a83c359fd6ee35f2059e5db1
victor-liu <jiaxinliu.victor@gmail.com>
2026-05-17 22:42:57 -0400

    optionally build b-smp example

--- expr/build_all.sh
+++ expr/build_all.sh
@@ -5,5 +5,7 @@ PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 echo "[BUILD] PROJECT_DIR is ${PROJECT_DIR}"
 
-cmake -S "${PROJECT_DIR}" -B "${PROJECT_DIR}/build" -DCMAKE_BUILD_TYPE=Release
+cmake -S "${PROJECT_DIR}" -B "${PROJECT_DIR}/build" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBSMP_BUILD_EXAMPLE=ON
 cmake --build "${PROJECT_DIR}/build" -j
